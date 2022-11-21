966a961f68784f31b4e04fb6b709191679f1da6e
waqas ahmed <37307194+Iswaqasahmed@users.noreply.github.com>
2022-11-22 01:10:27 +0500

    Added Decorator
    Decorator is a structural pattern that allows adding new behaviors to objects dynamically by placing them inside special wrapper objects, called decorators.
    
    Using decorators you can wrap objects countless number of times since both target objects and decorators follow the same interface. The resulting object will get a stacking behavior of all wrappers.

---
+++ Decorator/Decorator.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Concept.Decorator
+{
+
+    public abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+    public class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteComponent.Operation()");
+        }
+    }
+
+
+    public abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+    public class ConcreteDecoratorA : Decorator
+    {
+        private string addedState;
+
+        public override void Operation()
+        {
+            base.Operation();
+            addedState = "New State";
+            Console.WriteLine("ConcreteDecoratorA.Operation()");
+        }
+    }
+
+    public class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Console.WriteLine("ConcreteDecoratorB.Operation()");
+        }
+
+        public void AddedBehavior()
+        {
+
+        }
+    }
+
+    public class Client
+    {
+        public void ClientCode(Component component)
+        {
+            Console.WriteLine("RESULT: ");
+            component.Operation();
+        }
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            ConcreteComponent concreteComponent = new ConcreteComponent();
+            Console.WriteLine("Client: I've got a simple component:");
+            client.ClientCode(concreteComponent);
+            Console.WriteLine();
+
+            ConcreteDecoratorA decorator1 = new ConcreteDecoratorA();
+            decorator1.SetComponent(concreteComponent);
+            ConcreteDecoratorB decorator2 = new ConcreteDecoratorB();
+            decorator2.SetComponent(decorator1);
+            Console.WriteLine("Client: Now I've got a decorated component:");
+            client.ClientCode(decorator2);
+        }
+    }
+
+   
+}
